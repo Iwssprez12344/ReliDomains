ddc4dd0007ff7a6a024bdd7f391f50916456a6c1
Iwssprez12344 <cl34paliometros@hsonetone.clsd.net>
2026-08-22 23:58:43 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+function githubLogin() {
+window.location.href = "https://github.com/login"
+}
 
+function connectRepository() {
+alert("GitHub repository connection will be available once GitHub OAuth is configured.")
+}
+
+function addDomain() {
+const domain = prompt("Enter your domain")
+if (!domain) return
+alert(domain + " has been added to your ReliDomains project queue.")
+}
